81728bf969ce3ae7374b34dd9f8e0f7c30671469
Eduardo Moreira <eduardo.moreira@gmail.com>
2024-11-12 16:42:29 -0300

    header-color-2

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "Red" : "Black"}
+          color={showAdd ? "Blue" : "Red"}
           text={showAdd ? "Fechar" : "Adicionar"}
           onClick={onAdd}
         />
